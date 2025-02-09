c967faf760868f55cd5be31db8eed629c59cb2be
mjarecki1991 <mjarecki19991@gmail.com>
2025-02-09 13:53:14 +0100

    Updated by the logic of counting letters

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ def count_words(file_contents):
     counter = 0
 
     for word in words:
-        counter += 1
+        counter += 1 
 
     print(f"Total word count is: {counter}")
 
 
+    
 
+def count_characters(file_contents):
+    low_end = file_contents.lower()
+    my_dict = {}
+    
+    for character in low_end:
+        if character not in my_dict:
+            my_dict[character] = 1
+        else:
+            my_dict[character] += 1
 
+    print(f"{my_dict}")    
+    return my_dict
+    
+    
 
 
 
@@ -22,6 +36,7 @@ def main():
         file_contents = f.read()
         print_terminal(file_contents)
         count_words(file_contents)
+        count_characters(file_contents)
         
 if __name__ == "__main__":
     main()
