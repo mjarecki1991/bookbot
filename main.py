45b9ff6bbd15e062639d15ed145739b6a65d0726
mjarecki1991 <mjarecki19991@gmail.com>
2025-02-09 12:20:25 +0100

    Added the file reading and printing to console

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def print_terminal(file_contents):
+    print(file_contents)
+
+
+
+
+
+def main():
+
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print_terminal(file_contents)
+        
+if __name__ == "__main__":
+    main()
