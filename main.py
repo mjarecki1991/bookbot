66712841219a3a5e560a2637954ed11d1c12a76d
mjarecki1991 <mjarecki19991@gmail.com>
2025-02-09 12:49:17 +0100

    Updated by counter of words

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def print_terminal(file_contents):
     print(file_contents)
 
+def count_words(file_contents):
+    words = file_contents.split()
+    counter = 0
+
+    for word in words:
+        counter += 1
+
+    print(f"Total word count is: {counter}")
+
+
+
 
 
 
@@ -10,6 +21,7 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print_terminal(file_contents)
+        count_words(file_contents)
         
 if __name__ == "__main__":
     main()
